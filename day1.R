b33d8e6e32cb27c35d08fe6b2b6293bb1edc6767
Maria Ma <i.am.maria.ma@gmail.com>
2021-12-02 16:16:22 -0800

    Update day1.R
    added a base R solution for the first part, and a better tidyverse solutoin

--- day1.R
+++ day1.R
@@ -1,11 +1,18 @@
 #advent of code day 1
 library(tidyverse)
+advent_code <- read.table("day1_input")
 
-advent_code <- as.data.frame(advent_code_list)
+
 advent_code %>% 
-  mutate(change = advent_code_list - lag(advent_code_list)) %>% 
+  rename(nums = V1) %>%
+  mutate(change = nums - lag(nums)) %>% 
   mutate(inc = case_when(change > 0 ~ "increased") ) %>% 
   filter(inc == "increased") %>% nrow()
+
+# cleaner solution:
+advent_code %>% 
+  rename(nums = V1) %>%
+  filter(nums > lag(nums)) %>% nrow()
 
 
 advent_code %>% 
@@ -18,3 +25,19 @@ advent_code %>%
   filter(inc == "increased") %>% nrow()
 # there's definitely a more efficient way to do this lol
 
+
+# base R solution
+advent_code <- read.table("day1_input")
+lag <- c(NA, head(advent_code$V1, -1))
+advent_code <- cbind(advent_code, lag)
+
+
+advent_code$change = advent_code$V1 - advent_code$lag
+advent_code$increase = ifelse(advent_code$change > 0, "increase", "decrease")
+increase <- advent_code[advent_code$increase == "increase",]
+increase <- increase[!is.na(increase$increase),]
+nrow(increase)
+
+
+# hmm why doesn't this one work
+nrow(advent_code[advent_code$V1 > advent_code$lag,]) 
